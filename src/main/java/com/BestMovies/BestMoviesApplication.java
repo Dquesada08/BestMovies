06195211da32e231fee40114a2778747499949f2
Dquesada08 <91170905+Dquesada08@users.noreply.github.com>
2022-03-26 22:09:00 -0600

    BestMovies

---
+++ src/main/java/com/BestMovies/BestMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.BestMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestMoviesApplication.class, args);
+	}
+
+}
